028d446be58c6e72dac53e71898048eb64283a07
Vadims Gostevs <vadims.gostevs@gmail.com>
2021-03-02 12:25:54 +0200

    Class1 -> LineMaster2

--- src/lv/nsva/Class1.java
+++ src/lv/nsva/Class1.java
@@ -4,5 +4,6 @@ public class Class1 {
     public static void main(String[] args) {
         System.out.println("Line1");
         System.out.println("LineMaster");
+        System.out.println("LineMaster2");
     }
 }
