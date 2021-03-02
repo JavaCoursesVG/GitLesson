915e3e1ca30d41a7c0a6fb56c470162d6ccae8c3
Vadims Gostevs <vadims.gostevs@gmail.com>
2021-03-02 12:13:15 +0200

    Class1 -> Added line3, Main -> Question about age added

--- src/lv/nsva/Class1.java
+++ src/lv/nsva/Class1.java
@@ -1,8 +1,11 @@
 package lv.nsva;
 
+import java.sql.SQLOutput;
+
 public class Class1 {
     public static void main(String[] args) {
         System.out.println("Line1");
         System.out.println("Line2");
+        System.out.println("Line3");
     }
 }
